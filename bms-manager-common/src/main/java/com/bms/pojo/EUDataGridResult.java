bc86102a99029f7f59fa01b63ac415d59344c816
jimmy <13833611187@163.com>
2017-05-16 20:16:19 +0800

    提交GridResult.java文件

---
+++ bms-manager-common/src/main/java/com/bms/pojo/EUDataGridResult.java
@@ -0,0 +1,42 @@
+package com.bms.pojo;
+
+import java.util.List;
+
+public class EUDataGridResult {
+    private long total;  //总记录数
+    private List<?> rows;
+    private int pageCount;//总页数
+    private int curPage;//当前页
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+
+    public List<?> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<?> rows) {
+        this.rows = rows;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public int getCurPage() {
+        return curPage;
+    }
+
+    public void setCurPage(int curPage) {
+        this.curPage = curPage;
+    }
+}
